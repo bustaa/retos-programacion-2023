dd48d1f0159a79c007d45eb1a90f525670c66948
Miguel <mabusta.06@gmail.com>
2025-02-19 12:27:22 +0100

    Reto #0

---
+++ retos/Reto #0 - EL FAMOSO FIZZ BUZZ/Reto #0 - EL FAMOSO FIZZ BUZZ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Reto__0___EL_FAMOSO_FIZZ_BUZZ
+{
+    class Program
+    {
+        static string ObtenerFizzBuzz(int num)
+        {
+            if (num % 3 == 0)
+            {
+                if (num % 5 == 0)
+                {
+                    return "fizzbuzz";
+                }
+                else
+                {
+                    return "fizz";
+                }
+            }
+            else if (num % 5 == 0)
+            {
+                return "buzz";
+            }
+            else
+            {
+                return num.ToString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(ObtenerFizzBuzz(i));   
+            }
+        }
+    }
+}
